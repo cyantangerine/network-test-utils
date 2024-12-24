d95f0af99552903c2efa76aa181a021be8afbc15
Elvin <1487819688@qq.com>
2024-12-24 16:01:28 +0800

    url 12月更新

--- main gathering result.py
+++ main gathering result.py
@@ -6,8 +6,8 @@ from conf import RESULT_PATH
 
 df_domc = pd.read_csv(RESULT_PATH + "/firefox_domc_result.csv", encoding='gbk')
 df_fire = pd.read_csv(RESULT_PATH + "/firefox_result.csv", encoding='gbk')
-df_domc['url'] = df_domc['url'].str.replace('ebookcentral.proquest.com/auth/lib/ucas/', 'ebookcentral.proquest.com')
-df_fire['url'] = df_fire['url'].str.replace('ebookcentral.proquest.com/auth/lib/ucas/', 'ebookcentral.proquest.com')
+df_domc['url'] = df_domc['url'].str.replace('ebookcentral.proquest.com/lib/hit/', 'ebookcentral.proquest.com')
+df_fire['url'] = df_fire['url'].str.replace('ebookcentral.proquest.com/lib/hit/', 'ebookcentral.proquest.com')
 df_tcping = pd.read_csv(RESULT_PATH + "/tcping_result.csv", encoding='gbk')
 df_tracert = pd.read_csv(RESULT_PATH + "/tracert_result.csv", encoding='gbk', usecols=[0, 1])
 
